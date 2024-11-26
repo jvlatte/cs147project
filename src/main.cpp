d704cd9113288f1eb3328b44671d827681a0e4e7
Trystan Vasquez <trystanv@uci.edu>
2024-11-25 23:26:55 -0800

    implemented working haptic motor and pre-set vibration patterns

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,39 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_DRV2605.h>
 
-// put function declarations here:
-int myFunction(int, int);
+Adafruit_DRV2605 drv;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(9600);
+  Serial.println("DRV2605 Output Test");
+
+  // Initialize DRV.
+  if (!drv.begin()) {
+    Serial.println("Could not find DRV...");
+    while (1);
+  }
+  Serial.println("Found DRV!");
+
+  // Set the haptic mode for vibrating motor disc.
+  drv.selectLibrary(1);
+  // Set internal trigger mode.
+  drv.setMode(DRV2605_MODE_INTTRIG); 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  // Try preset effects 1 to 10. 
+  for (uint8_t effect = 1; effect <= 10; effect++) { 
+    Serial.print("Playing effect #");
+    Serial.println(effect);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    // Set effect for slot 0.
+    drv.setWaveform(0, effect); 
+    // End waveform with 0 to stop.
+    drv.setWaveform(1, 0);     
+  
+    // Play pattern then wait a second before the next effect.
+    drv.go(); 
+    delay(1000); 
+  }
 }
